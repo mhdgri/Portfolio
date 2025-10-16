9ceadbb4b887527be18ce23c1e592ee206657301
Mohamed Guerroui <mohamed.guerroui.mg@gmail.com>
2025-10-17 01:34:58 +0200

    Configuration tailwinds

--- tailwinds.config.js
+++ tailwinds.config.js
@@ -1,7 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  darkMode: 'class',
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
